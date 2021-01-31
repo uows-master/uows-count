18f85d9c487909543a16eea540ddaefcd552ad39
Saadi Save <saadisave@gmail.com>
2021-01-31 23:45:39 +0530

    Refactored the code

--- src/db.rs
+++ src/db.rs
@@ -1,28 +1,18 @@
 #![allow(dead_code)]
-use mongodb::{bson::doc, Client, Database};
-use tokio::{fs::OpenOptions, io::AsyncReadExt};
+use mongodb::{Client, Database};
 
-pub async fn init(cfilepath: &str) -> Database {
-    let mut file = OpenOptions::new().read(true).open(cfilepath).await.unwrap();
+pub async fn init(bind: ([u8; 4], u16), dbname: &str) -> Database {
+    let client = Client::with_uri_str(
+        format!(
+            "mongodb://{}.{}.{}.{}:{}",
+            bind.0[0], bind.0[1], bind.0[2], bind.0[3], bind.1
+        )
+        .as_str(),
+    )
+    .await
+    .unwrap();
 
-    let mut filecontent = String::new();
-    file.read_to_string(&mut filecontent).await.unwrap();
-
-    let client = Client::with_uri_str("mongodb://127.0.0.1:27017")
-        .await
-        .unwrap();
-
-    let db = client.database("UOWS");
-
-    let coll = db.collection("testone");
-
-    coll.delete_many(doc! {}, None).await.unwrap();
-
-    for i in filecontent.split('\n') {
-        coll.insert_one(doc! { "name": i, "votes": 0 }, None)
-            .await
-            .unwrap();
-    }
+    let db = client.database(dbname);
 
     db
 }
--- src/inits.rs
+++ src/inits.rs
@@ -1,1 +1,43 @@
+#![allow(dead_code)]
+use mongodb::{bson::doc, Database};
+use tokio::{fs::OpenOptions, io::AsyncReadExt};
+use uows_crypto::Data;
 
+pub async fn init_candidates(db: Database, clistfile: &str) {
+    let collection = db.collection("candidates");
+
+    let mut candidates = String::new();
+
+    OpenOptions::new()
+        .read(true)
+        .open(clistfile)
+        .await
+        .unwrap()
+        .read_to_string(&mut candidates)
+        .await
+        .unwrap();
+
+    for c in candidates.split('\n') {
+        collection
+            .insert_one(doc! {"name": c, "votes": 0}, None)
+            .await
+            .unwrap();
+    }
+}
+
+pub async fn init_enc(keyfile: &str) -> Data {
+    let mut keystr = String::new();
+
+    OpenOptions::new()
+        .read(true)
+        .open(keyfile)
+        .await
+        .unwrap()
+        .read_to_string(&mut keystr)
+        .await
+        .unwrap();
+
+    let keyvec: Vec<&str> = keystr.split('\n').collect();
+
+    Data::new(keyvec[0], keyvec[1])
+}
