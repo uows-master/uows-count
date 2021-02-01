66d6501fcc4bd6ee1cf450908bf90d2c810fde03
Saadi Save <saadisave@gmail.com>
2021-02-01 16:52:18 +0530

    Added example

--- src/inits.rs
+++ src/inits.rs
@@ -1,21 +1,12 @@
 #![allow(dead_code)]
 use mongodb::{bson::doc, Database};
-use tokio::{fs::OpenOptions, io::AsyncReadExt};
+use tokio::fs::read_to_string;
 use uows_crypto::Data;
 
-pub async fn init_candidates(db: Database, clistfile: &str) {
+pub async fn init_candidates(db: &Database, clistfile: &str) {
     let collection = db.collection("candidates");
 
-    let mut candidates = String::new();
-
-    OpenOptions::new()
-        .read(true)
-        .open(clistfile)
-        .await
-        .unwrap()
-        .read_to_string(&mut candidates)
-        .await
-        .unwrap();
+    let candidates = read_to_string(clistfile).await.unwrap();
 
     for c in candidates.split('\n') {
         collection
@@ -26,18 +17,9 @@ pub async fn init_candidates(db: Database, clistfile: &str) {
 }
 
 pub async fn init_enc(keyfile: &str) -> Data {
-    let mut keystr = String::new();
+    let keystr = read_to_string(keyfile).await.unwrap();
 
-    OpenOptions::new()
-        .read(true)
-        .open(keyfile)
-        .await
-        .unwrap()
-        .read_to_string(&mut keystr)
-        .await
-        .unwrap();
+    let kvec: Vec<&str> = keystr.split('\n').collect();
 
-    let keyvec: Vec<&str> = keystr.split('\n').collect();
-
-    Data::new(keyvec[0], keyvec[1])
+    Data::new(kvec[0], kvec[1])
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
 mod db;
 mod inits;
 
+use mongodb::bson::doc;
+use tokio::time::Instant;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let now = Instant::now();
+
+    // Example init
+    let dbase = db::init(([127, 0, 0, 1], 27017), "UOWS").await;
+    inits::init_candidates(&dbase, "clist.txt").await;
+    
+    let reciept = dbase
+        .collection("candidates")
+        .find(doc! {}, None)
+        .await
+        .unwrap();
+
+    let then = now.elapsed();
+    
+    println!("{:#?}", reciept);
+    println!("Time: {:?}", then);
 }
